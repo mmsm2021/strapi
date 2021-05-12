ceeebef2e0843e4f37635245e52235b101db8748
Martin René Sørensen <git@sicet7.com>
2021-05-12 14:51:45 +0200

    init strapi.

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '60819e6e3fc30efc79d0685ad8352b84'),
+    },
+  },
+});
